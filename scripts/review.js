6b3c50b2b69a561a4d63947caececb9bad3ce9e7
Victor Liu <vctl95@gmail.com>
2023-03-07 09:50:12 -0800

    Add function to grab hike name ID

---
+++ scripts/review.js
@@ -0,0 +1,14 @@
+var hikeDocID = localStorage.getItem("hikeDocID");    //visible to all functions on this page
+console.log(hikeDocID);
+
+function getHikeName(id) {
+    db.collection("hikes")
+        .doc(id)
+        .get()
+        .then((thisHike) => {
+            var hikeName = thisHike.data().name;
+            document.getElementById("hikeName").innerHTML = hikeName;
+        });
+}
+
+getHikeName(hikeDocID);
